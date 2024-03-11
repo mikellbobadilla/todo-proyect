2be4c6ded16622a09b5cc07e05901500fa61646a
Sting <bobadillasting@gmail.com>
2024-03-11 20:48:44 -0300

    Add error handling and toast notification in ListOfTodos component

--- front-todo-app/src/components/ListOfTodos.tsx
+++ front-todo-app/src/components/ListOfTodos.tsx
@@ -1,3 +1,4 @@
+import { toast } from 'sonner'
 import { useDeleteTodoMutation, useGetTodosQuery } from '../store/todoApi/todoApi'
 import { Badge } from './Badge'
 import { Spinner } from './Spinner'
@@ -8,18 +9,22 @@ export function ListOfTodos() {
     const { data, isLoading, isError } = useGetTodosQuery()
     const [deleteTodo] = useDeleteTodoMutation()
 
-    if (isError) return (
-        <section>
-            <p className='text-2xl text-center mt-4 text-red-300'>Something went wrong</p>
-        </section>
-    )
-
-    if (isLoading)
+    if (isError) {
+        toast.error(' Check your internet connection and try again.')
         return (
-            <section className='flex justify-center mt-10'>
-                <Spinner size='size-10' />
+            <section>
+                <p className='text-2xl text-center mt-4 text-red-300'>
+                    Something went wrong
+                </p>
             </section>
         )
+    }
+
+    if (isLoading) return (
+        <section className='flex justify-center mt-10'>
+            <Spinner size='size-10' />
+        </section>
+    )
 
     return (
         <section >
@@ -33,13 +38,25 @@ export function ListOfTodos() {
                 </p>
             </header>
             <div>
-                <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3'>
-                    {
-                        data?.content.map((item) => (
-                            <TodoCard todo={item} deleteTodo={() => deleteTodo(item.id)} />
-                        ))
-                    }
-                </ul>
+                {
+                    data?.content.length === 0 ? (
+                        <p className='text-center text-xl mt-4 text-gray-600'>
+                            No todos found, add a new one.
+                        </p>
+                    ) : (
+                        <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3'>
+                            {
+                                data?.content.map((item) => (
+                                    <TodoCard
+                                        key={item.id}
+                                        todo={item}
+                                        deleteTodo={() => deleteTodo(item.id)}
+                                    />
+                                ))
+                            }
+                        </ul>
+                    )
+                }
             </div>
         </section>
     )
