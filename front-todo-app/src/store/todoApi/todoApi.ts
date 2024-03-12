1e2dda8561d83c384ef071e0d17c7703a0d1d9a0
Sting <bobadillasting@gmail.com>
2024-03-12 14:07:48 -0300

    Refactor todoApi.ts to include pagination in getTodos query

--- front-todo-app/src/store/todoApi/todoApi.ts
+++ front-todo-app/src/store/todoApi/todoApi.ts
@@ -6,16 +6,27 @@ export const todoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: '/server/api' }),
     tagTypes: ['GetTodos'],
     endpoints: builder => ({
-        getTodos: builder.query<TodoResponse, void>({
-            query: () => '/todos',
-            providesTags: ['GetTodos']
+        getTodos: builder.query<TodoResponse, number | void>({
+            query: (page = 1) => `/todos?page=${page}`,
+            providesTags: (result) =>
+                result ? [
+                    // Provides a tag for each post in the current page,
+                    // as well as the 'PARTIAL-LIST' tag.
+                    ...result.content.map(({ id }) => ({ type: 'GetTodos' as const, id })),
+                    { type: 'GetTodos', id: 'PARTIAL-LIST' },
+                ]
+                    : [{ type: 'GetTodos', id: 'PARTIAL-LIST' }]
+
         }),
         deleteTodo: builder.mutation<void, TodoId>({
             query: (id) => ({
                 url: `/todos/${id}`,
                 method: 'DELETE'
             }),
-            invalidatesTags: ['GetTodos']
+            invalidatesTags: (_, error, id) => [
+                { type: 'GetTodos', id },
+                { type: 'GetTodos', id: 'PARTIAL-LIST' },
+            ]
         }),
         createTodo: builder.mutation<TodoWithId, Todo>(
             {
