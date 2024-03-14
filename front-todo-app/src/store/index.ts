f25b43f8fe505c88a2aff70195b1cfd846fa9e85
Sting <bobadillasting@gmail.com>
2024-03-13 23:33:54 -0300

    Add error handling middleware to Redux store

--- front-todo-app/src/store/index.ts
+++ front-todo-app/src/store/index.ts
@@ -1,9 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { Middleware, configureStore, isRejectedWithValue } from '@reduxjs/toolkit'
+import { toast } from 'sonner'
 import { todoApi } from './todoApi/todoApi'
+
+const rtkQueryErrorLogger: Middleware = () => (next) => (action) => {
+    if (isRejectedWithValue(action)) {
+        if (action.payload && (action.payload as { status: number })?.status === 500) {
+            toast.error(`Opps maybe you don't have internet connection!`)
+        } else {
+            toast.warning(`Ocurrió un error!`)
+        }
+    }
+    return next(action)
+}
 
 export const store = configureStore({
     reducer: {
         [todoApi.reducerPath]: todoApi.reducer
     },
-    middleware: getMiddleware => getMiddleware().concat(todoApi.middleware)
+    middleware: getMiddleware => getMiddleware().concat(rtkQueryErrorLogger, todoApi.middleware)
 })
