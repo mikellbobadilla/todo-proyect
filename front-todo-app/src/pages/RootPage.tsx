5b866252828a608c1d9891ef04880c379d0c2c6b
Sting <bobadillasting@gmail.com>
2024-03-13 23:33:41 -0300

    Add Toaster component to RootPage

--- front-todo-app/src/pages/RootPage.tsx
+++ front-todo-app/src/pages/RootPage.tsx
@@ -8,8 +8,8 @@ export function RootPage() {
             <Header />
             <main className='px-3 py-2 max-w-7xl mx-auto w-full'>
                 <Outlet />
+                <Toaster richColors position='top-center' visibleToasts={1} expand />
             </main>
-            <Toaster richColors position='top-center' />
         </>
     )
 }
